5b50334ddff296fdae38e54603780c6cf6030c8f
thadeus <thadeusnyariki94@gmail.com>
2024-03-19 01:33:10 +0300

    first commit

---
+++ 01-variables/main.py
@@ -0,0 +1,25 @@
+# variable = a container for a value. Behaves as the value that it contains
+
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name + " " + last_name
+
+print("Hello " + full_name)
+# print(type(name))
+
+age = 21
+age = age + 1
+age += 1
+print(age)
+print("Your age is: " + str(age))
+print(type(age))
+
+height = 250.5
+print(height)
+print("Your height is: " + str(height) + "cm")
+print(type(height))
+
+human = False
+print(human)
+print("Are you a human: " + str(human))
+print(type(human))
